a2e6ba8671f244a015db1bf016de812eb40e5205
tharsus.proux <tharsus@poli.ufrj.br>
2020-08-01 15:22:30 -0300

    Created the project and prepared the basics

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import winston from 'winston';
+
+import gradesRouter from './routes/grades.js';
+
+import { promises as fs } from 'fs';
+const { readFile, writeFile } = fs;
+
+global.fileName = 'grades.json';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'my-bank-api.log' }),
+  ],
+  format: combine(label({ label: 'my-label-api' }), timestamp(), myFormat),
+});
+
+const app = express();
+app.use(express.json());
+
+app.use('/grades', gradesRouter);
+
+app.listen(3000, async () => {
+  try {
+    await readFile(global.fileName);
+    global.logger.info('API started.');
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      grades: [],
+    };
+
+    await writeFile(global.fileName, JSON.stringify(initialJson, null, '\t'))
+      .then(() => {
+        global.logger.info('API started and file created.');
+      })
+      .catch((err) => {
+        global.logger.error(err.message);
+      });
+  }
+});
